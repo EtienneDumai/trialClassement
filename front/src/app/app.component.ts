4bfce8bf82b2879469c13388943ea586be1dec66
EtienneDumai <etienne.dumai@gmail.com>
2025-02-17 22:04:47 +0100

    ajout du composant header ajouté a la page d'accueil

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
+import { AccueilComponent } from './pages/accueil/accueil.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, AccueilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- front/src/app/pages/accueil/accueil.component.ts
+++ front/src/app/pages/accueil/accueil.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../component/header/header.component';
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.css'
 })
