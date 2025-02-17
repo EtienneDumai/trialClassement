adf7a668297b64c80c5185e1d81b76e5435c83f1
EtienneDumai <etienne.dumai@gmail.com>
2025-02-17 22:27:01 +0100

    building premieres routes entre pages

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { ClassementComponent } from './pages/classement/classement.component';
+export const routes: Routes = [
+    { path: '', component: AccueilComponent },
+    { path: 'classement', component: ClassementComponent },
+];
+export const appRoutingProviders = [
+    provideRouter(routes)
+];
