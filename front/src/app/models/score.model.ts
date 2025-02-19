53c7e971be55efd1ea6746ef0ca7054a736d328f
EtienneDumai <etienne.dumai@gmail.com>
2025-02-19 21:35:18 +0100

    fix(.gitignore global): modif du gitignore

---
+++ front/src/app/models/score.model.ts
@@ -0,0 +1,10 @@
+import { Pilote } from "./pilote.model";
+import { Tour } from "./tour.model";
+
+export class Score {
+    tabSore: Pilote[];
+    constructor(tabSore: Pilote[]) {
+        this.tabSore = tabSore;
+    }
+    
+}
